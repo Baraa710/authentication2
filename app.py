c43a3b830f4be9698a5cb5ff92afbdd79c95caa7
Baraa710 <baraalchalabi@gmail.com>
2023-05-30 12:57:32 +0300

    Implement dashboard page and logout functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user,current_user
 from flask_wtf import FlaskForm
@@ -38,9 +38,11 @@ class RegisterForm(FlaskForm):
             username=username.data).first()
 
         if existing_user_username:
+            
             raise ValidationError(
             "That username already exists, please choose a different one."
             )
+            
         
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,24 +55,44 @@ def home():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
-    
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
     return render_template('login.html', form = form)
+
+
 
 @app.route('/dashboard', methods = ['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/logout', methods= ['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))  
+
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
+        
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password = hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
+
+    
+
     return render_template('register.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
